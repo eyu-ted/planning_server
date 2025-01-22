b93919e15fe1f1029c3a5201d17c5a3379636690
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2025-01-22 10:04:02 +0300

    Update plan_route.go

--- delivery/route/plan_route.go
+++ delivery/route/plan_route.go
@@ -44,7 +44,7 @@ func NewPlanRouter(env *config.Env, timeout time.Duration, db database.Database,
 	group.GET("/announcements", sc.GetAllAnnouncements)
 	group.DELETE("/announcements/:id",sc.DeleteAnnouncement)
 
-	group.GET("/user/plan-and-report", sc.GetUserPlansAndReports)
+	group.POST("/user/plan-and-report", sc.GetUserPlansAndReports)
 
 	// group.GET("/plan", sc.GetPlan)
 	// group.PUT("/plan", sc.EditPlan)
